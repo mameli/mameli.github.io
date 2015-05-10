88cfef0fd3474d784ee8b3c3ddb782656751364c
mameli <mameli93@gmail.com>
2015-05-11 01:53:27 +0200

    grid-game added

--- javascripts/jquery.js
+++ javascripts/jquery.js
@@ -1,15 +1,36 @@
 $( document ).ready(function() {
 
-$( "#forkme_banner" ).on("mouseenter",function( event ) {
+  $( "#forkme_banner" ).on("mouseenter",function( event ) {
     $(this).animate({"padding-top":"30px","background-position-y":"70%"});
 
 
 
-});
+  });
 
-$( "#forkme_banner" ).on("mouseleave",function( event ) {
+  $( "#forkme_banner" ).on("mouseleave",function( event ) {
     $(this).animate({"padding-top":"10px","background-position-y":"50%"});
 
-});
+  });
+
+  var rowNum;
+  var grid_width=$(".grid-game").width();
+  for (var i=0;i<5;i++){
+    rowNum="row"+i;
+    $(".grid-game").append("<div id='"+rowNum+"' class='row'></div>");
+    for(var j=0;j<grid_width/25;j++){
+      $("#"+rowNum).append("<div class='col'></div>");
+    }
+  }
+  $(".col").width(grid_width/50);
+  $(".col").height(grid_width/50);
+  changeColor();
 
 });
+
+
+var changeColor=function(){
+    $(".col").on("mouseenter",function(event){
+        //$(this).addClass("black");
+        $(this).css("opacity", $(this).css("opacity") * .75);
+    });
+}
